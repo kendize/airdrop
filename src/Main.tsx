0e0e2b683c20069a0290e94892b2ed5c1d544b0a
yurii-okereshko <yurii.okereshko@honeycombsoft.com>
2024-01-27 04:52:30 +0200

    adaptive-draft

--- src/Main.tsx
+++ src/Main.tsx
@@ -32,9 +32,7 @@ function Main() {
 
   return (
     <div className="app">
-      <div className="bg">
-
-      </div>
+      <div className="bg" />
       <div className='background'>
         <div className='rainbow'>
           <img src={rainbow}></img>
@@ -67,128 +65,134 @@ function Main() {
           </div>
         </div>
         <div className='award-container'>
-          <div className='award-abs'>
-            <div className='award-block'>
+          <div className='award-block'>
 
-              <div className='timer-border'>
-                <div className='timer'>
-                  0:0{minutes}:{seconds < 10 ? '0' + seconds : seconds}
-                </div>
+            <div className='timer-border'>
+              <div className='timer'>
+                0:0{minutes}:{seconds < 10 ? '0' + seconds : seconds}
+              </div>
+            </div>
+
+            <div className='drift-label'>
+              <div className='drift' data-content='10000 $DRIFT'>
+                10000 $DRIFT
               </div>
 
-              <div className='drift-label'>
-                <div className='drift' data-content='10000 $DRIFT'>
-                  10000 $DRIFT
-                </div>
+            </div>
+            <div className='whitelist-label'>
+              WHITELIST AIRDROP
+            </div>
+            <div className='about-label'>
+              Your wallet is whitelisted. DriftCoin is a universal decentralized identity protocol that connects people, assets, and dApps across different blockchains. Drift creates open-source, decentralized, universal identities.
+            </div>
+            <button className='get-award-button'>
+              <div className='get-award-text'>
+                GET AWARD NOW
 
               </div>
-              <div className='whitelist-label'>
-                WHITELIST AIRDROP
-              </div>
-              <div className='about-label'>
-                Your wallet is whitelisted. DriftCoin is a universal decentralized identity protocol that connects people, assets, and dApps across different blockchains. Drift creates open-source, decentralized, universal identities.
-              </div>
-              <button className='get-award-button'>
-                <div className='get-award-text'>
-                  GET AWARD NOW
-
-                </div>
-              </button>
-            </div>
-          </div >
-        </div>
-
-        <div className='participate-container' id='participate-container'>
-          <div className='participate-label'>
-            <div className='participate-gradient'>
-              We allow a limited number
-            </div>
-            <div className='participate-gradient'>
-              of tokens to be minted, our first
-
-            </div>
-            <div>
-              airdrop is whitelist only.
-            </div>
+            </button>
           </div>
-          <div className='participate'>
-            <div className='participate-left'>
-              <div id="cells">
-                <div className='participate-outer-cell'>
-                  <div className='participate-inner-cell'>
-                    <img src={participate}></img>
-                  </div>
-                </div>
-                <div className='participate-outer-cell'>
-                  <div className='participate-inner-cell'>
-                    <img src={participate}></img>
-                  </div>
-                </div>
-                <div className='participate-outer-cell'>
-                  <div className='participate-inner-cell'>
-                    <img src={participate}></img>
-                  </div>
-                </div>
-              </div>
+        </div >
 
-              <div className='participate-header'>
-                Exclusive Airdrop of Drift tokens
+
+      </div>
+
+      <div className='participate-container' id='participate-container'>
+        <div className='participate-label'>
+          <div className='participate-gradient'>
+            We allow a limited number
+          </div>
+          <div className='participate-gradient'>
+            of tokens to be minted, our first
+
+          </div>
+          <div>
+            airdrop is whitelist only.
+          </div>
+        </div>
+        <div className='participate'>
+          <div className='participate-left'>
+            <div id="cells">
+              <div className='participate-outer-cell'>
+                <div className='participate-inner-cell'>
+                  <img src={participate}></img>
+                </div>
               </div>
-              <div className='participate-text'>
-                <div>
-                  Gain access to an exclusive airdrop of Drift tokens,
-                  a limited number given to members of our drop project.
-                  These tokens open up additional opportunities on the
-                  decentralized trading platform.
+              <div className='participate-outer-cell'>
+                <div className='participate-inner-cell'>
+                  <img src={participate}></img>
+                </div>
+              </div>
+              <div className='participate-outer-cell'>
+                <div className='participate-inner-cell'>
+                  <img src={participate}></img>
                 </div>
               </div>
             </div>
-            <div className='participate-right'>
+
+            <div className='participate-header'>
+              Exclusive Airdrop of Drift tokens
+            </div>
+            <div className='participate-text'>
               <div>
+                Gain access to an exclusive airdrop of Drift tokens,
+              </div>
+              <div>
+                a limited number given to members of our drop project.
+              </div>
+              <div>
+                These tokens open up additional opportunities on the
+              </div>
+              <div>
+                decentralized trading platform.
+              </div>
+            </div>
+          </div>
+          <div className='participate-right'>
+            <div>
 
+            </div>
+            <img src={priority}></img>
+            <div className='participate-header'>
+              <span>
+                Priority Access to New
+              </span>
+              <span>
+                Features and Enhancements
+              </span>
+            </div>
+            <div className='participate-text'>
+              <div>
+                Drift token holders received via airdrop will have
               </div>
-              <img src={priority}></img>
-              <div className='participate-header'>
-                <span>
-                  Priority Access to New
-                </span>
-                <span>
-                  Features and Enhancements
-                </span>
+              <div>
+                benefits when accessing new features and updates
               </div>
-              <div className='participate-text'>
-                <div>
-                  Drift token holders received via airdrop will have
-                </div>
-                <div>
-                  benefits when accessing new features and updates
-                </div>
-                <div>
-                  on the Drift platform.
-                </div>
+              <div>
+                on the Drift platform.
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className='instruction' id="instruction">
-          <div className='instruction-header'>
-            <div className='instruction-gradient'>How to Participate </div>
-            <div>in the AirDrop</div>
+      <div className='instruction' id="instruction">
+        <div className='instruction-header'>
+          <div className='instruction-gradient'>How to Participate </div>
+          <div>in the AirDrop</div>
+        </div>
+        <div className='instructions-pics'>
+          <div className='instructions-item'>
+            <img src={image_1}></img>
+            Connect Wallet!
           </div>
-          <div className='instructions-pics'>
-            <div className='instructions-item'>
-              <img src={image_1}></img>
-              Connect Wallet!
-            </div>
-            <div className='instructions-item'>
-              <img src={image_2}></img>
-              Mint your drop!
-            </div>
-            <div className='instructions-item'>
-              <img src={image_3}></img>
-              Get the award!
-            </div>
+          <div className='instructions-item'>
+            <img src={image_2}></img>
+            Mint your drop!
+          </div>
+          <div className='instructions-item'>
+            <img src={image_3}></img>
+            Get the award!
           </div>
         </div>
       </div>
